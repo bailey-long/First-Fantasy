dccc0ea4424d2be829408426e76717703c25e304
Bailey Long <bailey-long@live.nmit.ac.nz>
2023-07-31 14:45:10 +1200

    Added boilerplate GUI classes for each game state

---
+++ First Fantasy/Classes/Interface/Main Menu/GUI_Main_Menu.cs
@@ -0,0 +1,17 @@
+﻿using Myra.Graphics2D.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace First_Fantasy.Classes.Interface.Main_Menu
+{
+    partial class GUI_Main_Menu: Grid
+    {
+        private void UI_LoadContent()
+        {
+
+        }
+    }
+}
